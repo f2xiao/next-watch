dc8effd0b9d38103349d600155eea6a9c46f337d
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-04-29 11:05:23 -0400

    edit .gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import './App.scss'
-
+import "./App.scss";
+import NextWatchDetailsPage from "./pages/NextWatchDetailsPage/NextWatchDetailsPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
-
   return (
     <>
-	NextWatch
+      <BrowserRouter className="app">
+        <Routes>
+          <Route path="/" element={<NextWatchListPage />} />
+          <Route path="/nextwatches" element={<NextWatchListPage />} />
+          <Route path="nextwatches/:Id" element={<NextWatchDetailsPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="*"
+            element={<div className="app__wrapper">Not Found 404 🥲</div>}
+          />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/NextWatchListPage/NextWatchListPage.tsx
@@ -0,0 +1,7 @@
+import  from "./NextWatchListPage.scss";
+
+const NextWatchListPage = () => {
+  return <div>NextWatchListPage</div>;
+};
+
+export default NextWatchListPage;
