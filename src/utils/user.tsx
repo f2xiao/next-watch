62e7bf48982abeb640d528d754a19c685b349b2e
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-05-07 08:50:05 -0400

    refactor user.tsx

--- src/utils/user.tsx
+++ src/utils/user.tsx
@@ -12,3 +12,17 @@ export const updateShare = async () => {
 export const getAllShared = async () => {
   return await axios.get(`${baseUrl}/allshared`, config);
 };
+
+export const loginUser = async (obj: {
+  username: string;
+  password: string;
+}) => {
+  return await axios.post(`${baseUrl}/login`, obj);
+};
+export const signupUser = async (obj: {
+  username: string;
+  password: string;
+  email: string;
+}) => {
+  return await axios.post(`${baseUrl}/signup`, obj);
+};
