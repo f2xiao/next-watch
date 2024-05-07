eeb0887b332781fc9e1b6c93c92cecb2dd13a244
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-05-07 15:58:49 -0400

    edit API_URL for deployment

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:8080";
+export const API_URL = "https://next-watch-backend.onrender.com";
 const setToken = (newToken: string | null) => {
   return `Bearer ${newToken}`;
 };
