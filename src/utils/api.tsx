700bafd6a32fc34384d4980001616c663b97a41a
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-05-07 17:46:51 -0400

    remove package

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,4 +1,5 @@
-export const API_URL = "https://next-watch-backend.onrender.com";
+export const API_URL = "http://localhost:8080";
+// export const API_URL = "https://next-watch-backend.onrender.com"; deployed backend url
 const setToken = (newToken: string | null) => {
   return `Bearer ${newToken}`;
 };
