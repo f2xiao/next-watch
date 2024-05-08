58b3c6fb224eaeaf9c5857408424556ea2dc3563
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-05-08 12:03:36 -0400

    update action after login

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -3,11 +3,14 @@ import "./LoginPage.scss";
 import UserForm from "../../components/UserForm/UserForm";
 import { login } from "../../utils/auth";
 import Banner from "../../components/Banner/Banner";
+// import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
+  // const navigate = useNavigate();
   return (
     <div className="login-page">
       <h1>Login</h1>
+      {/* <UserForm type="login" handleSubmit={login(navigate)} /> */}
       <UserForm type="login" handleSubmit={login()} />
       <Banner />
     </div>
--- src/utils/auth.tsx
+++ src/utils/auth.tsx
@@ -6,31 +6,34 @@ type User = {
   password: string;
 };
 
-export const login = () => async (event: React.SyntheticEvent, user: User) => {
-  event.preventDefault();
-  console.log(user);
-  const { username, password } = user;
-  //login the user
-  try {
-    const response = await loginUser({
-      username,
-      password,
-    });
+export const login =
+  // (navigate: (route: string) => void) =>
+  () => async (event: React.SyntheticEvent, user: User) => {
+    event.preventDefault();
+    console.log(user);
+    const { username, password } = user;
+    //login the user
+    try {
+      const response = await loginUser({
+        username,
+        password,
+      });
 
-    if (response.status === 401) {
-      console.log("Invalid username or password");
-    }
+      if (response.status === 401) {
+        console.log("Invalid username or password");
+      }
 
-    if (response.status === 200) {
-      // save the token to the localStorage
-      // console.log(response.data);
-      localStorage.setItem("authToken", response.data.token);
-      window.location.reload();
+      if (response.status === 200) {
+        // save the token to the localStorage
+        // console.log(response.data);
+        localStorage.setItem("authToken", response.data.token);
+        window.location.reload();
+        // navigate("/watches");
+      }
+    } catch (error) {
+      console.log("Request failed", error);
     }
-  } catch (error) {
-    console.log("Request failed", error);
-  }
-};
+  };
 
 export const signup =
   (navigate: (route: string) => void) =>
