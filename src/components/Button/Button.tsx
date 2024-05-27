f499ca8ba3424148e2c088ddf645ab2dbc45c6a9
fangfang <25191617+f2xiao@users.noreply.github.com>
2024-05-27 15:56:37 -0400

    add default value for className

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,14 @@
 import "./Button.scss";
 
 type PropTypes = {
+  className?: string;
   text: string;
   handleClick: () => void;
 };
 
-const Button = ({ text, handleClick }: PropTypes) => {
+const Button = ({ className = "button", text, handleClick }: PropTypes) => {
   return (
-    <button className="button" onClick={handleClick}>
+    <button className={className} onClick={handleClick}>
       {text}
     </button>
   );
