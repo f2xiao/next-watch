6a0738000558f894ec8ec91afbdadb2cb72e10c6
fangfang <25191617+f2xiao@users.noreply.github.com>
2024-06-24 20:25:16 -0400

    add div container for styling

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -21,13 +21,25 @@ const LoginPage = ({ isLoggedIn }: PropTypes) => {
   });
   return (
     <div className="login-page">
-      <h1>Login</h1>
-      {/* <UserForm type="login" handleSubmit={login(navigate)} /> */}
-      <UserForm type="login" handleSubmit={login()} />
-      <Banner />
-        <div className="login-page__cta">
-          <Button text="login as test acct" handleClick={loginTestUser()}/>
+      <div className="login-page__top">
+        <div  className="login-page__box">
+          <h1>Login</h1>
+          {/* <UserForm type="login" handleSubmit={login(navigate)} /> */}
+          <UserForm type="login" handleSubmit={login()} />
+          <div className="login-page__banner login-page__banner--first">
+            <Banner />
+          </div>
         </div>
+         <p className="login-page__text">OR</p>
+        <div className="login-page__box login-page__box--right">
+          <div className="login-page__cta">
+            <Button className="login-page__button" text="try with test acct" handleClick={loginTestUser()}/>
+          </div>
+        </div>
+      </div>
+      <div className="login-page__banner login-page__banner--second">
+        <Banner />
+      </div>
     </div>
   );
 };
