03334918359be53461bcd590846efbff0d9032eb
arashi4ever <25191617+f2xiao@users.noreply.github.com>
2024-08-26 17:01:41 -0400

    add HomeSection component to LoginPage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,34 +1,17 @@
 import { useEffect } from "react";
 import { useNavigate, useLocation, Outlet } from "react-router-dom";
 
-// type User = {
-//   username: string;
-//   emial: string;
-//   share: boolean;
-//   nextwatches: [];
-//   id: string;
-// };
-
-// type PropTypes = {
-//   user: User | null;
-// };
-
 type PropTypes = {
   isLoggedIn: boolean;
 };
 
-// const HomePage = ({ user }: PropTypes) => {
 const HomePage = ({ isLoggedIn }: PropTypes) => {
   const navigate = useNavigate();
   const location = useLocation();
 
   useEffect(() => {
-    // const authToken = localStorage.getItem("authToken");
-    // console.log(user);
-    // console.log(location.pathname);
-
     if (!isLoggedIn) {
-      navigate("/login");
+      navigate("/");
     } else {
       if (location.pathname === "/") {
         navigate("/watches");
@@ -36,7 +19,6 @@ const HomePage = ({ isLoggedIn }: PropTypes) => {
         navigate(`${location.pathname}`);
       }
     }
-    // }, [navigate, user, location.pathname]);
   }, [navigate, isLoggedIn, location.pathname]);
 
   return <Outlet />;
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -6,6 +6,11 @@ import Banner from "../../components/Banner/Banner";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import Button from "../../components/Button/Button";
+import HomeSection from "../../components/HomeSection/HomeSection";
+import whatUrl from "../../assets/images/home_what.png"
+import howUrl from "../../assets/images/home_how.png"
+import shareUrl from "../../assets/images/home_share.png"
+import logoUrl from "../../assets/logo/nextwatch-white.svg"
 
 type PropTypes = {
   isLoggedIn: boolean;
@@ -13,6 +18,26 @@ type PropTypes = {
 
 const LoginPage = ({ isLoggedIn }: PropTypes) => {
   const navigate = useNavigate();
+  const introductions=[
+    {
+      text:"What to do with NextWatch?",
+      title:"browse and watch trailers",
+      imgUrl:whatUrl
+    },
+    {
+      text:"How to use it?",
+      title:"create nextwatch list",
+      imgUrl:howUrl,
+      reverse:true
+    },
+    {
+      text:"Why nextwatch?",
+      title:"Share",
+      imgUrl: shareUrl
+    }
+  ]
+
+
 
   useEffect(() => {
     if (isLoggedIn) {
@@ -20,7 +45,21 @@ const LoginPage = ({ isLoggedIn }: PropTypes) => {
     }
   });
   return (
-    <div className="login-page">
+     <div className="login-page">
+      <nav style={{display:"flex", justifyContent:"space-between"}}>
+
+        <a href="#">
+          <img className="login-page__logo" style={{width:"2.5rem"}} src={logoUrl} alt="logo"  />
+        </a>
+        <div style={{display:"flex"}}>
+          <a href="#login">Login</a>
+          <a href="#signup">SignUp</a>
+        </div>
+      </nav>
+      {introductions.map(({title, imgUrl, text, reverse})=>(
+        <HomeSection reverse={reverse || false} sectionTitle={title} imgUrl={imgUrl} text={text}/>
+      ))
+      }
       <div className="login-page__top">
         <div  className="login-page__box">
           <h1>Login</h1>
@@ -41,6 +80,8 @@ const LoginPage = ({ isLoggedIn }: PropTypes) => {
         <Banner />
       </div>
     </div>
+  
+
   );
 };
 
